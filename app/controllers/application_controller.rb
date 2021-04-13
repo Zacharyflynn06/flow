fb7bf31e428168cb163dd2401a849df3c8779cad
Zac <zacharyflynn06@gmail.com>
2021-04-12 19:07:41 -0600

    add global helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
 
-    
+    def current_user
+        session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to controller: 'sessions', action: 'new' unless current_user
+    end
+
 end
