85b02e42b81c842fbdda0c3f0bbc2e393721fdfd
Zac <zacharyflynn06@gmail.com>
2021-04-12 23:07:30 -0600

    fix link sytles

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,7 +9,7 @@ class Course < ApplicationRecord
   validates :name, presence: true
   validates :day, presence: true, inclusion: %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
   validates :time, presence: true
-  validates :duration, presence: true, inclusion: %w("30" "60" "90")
+  # validates :duration, presence: true, inclusion: %w("30" "60" "90")
   validates :price, presence: true
 
 end
