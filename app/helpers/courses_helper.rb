3166b06ce63f35bdc561cfd657d4c4085f3c7b38
Zac <zacharyflynn06@gmail.com>
2021-04-21 11:56:44 -0600

    add course helpers for index

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,3 +1,22 @@
 module CoursesHelper
+
+
+    def conditionally_display_heading(user)
+        if params[:user_id]
+            tag.h1 user.full_name
+        else
+            tag.h1 'Here are our courses'
+        end
+    end
+
     
+    def create_new_course_if_admin
+        if logged_in? && current_user.admin?
+            tag.div class: "button" do
+                link_to 'Create a new Course', new_course_path
+            end
+            # content_tag(:div, link_to 'Create a new Course', new_course_path, class: "button" )
+        end
+    end
+
 end
