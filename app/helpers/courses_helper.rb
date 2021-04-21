77bc2e5482c988f2aedc9e8afccb3522418191e0
Zac <zacharyflynn06@gmail.com>
2021-04-21 12:18:31 -0600

    refactor new + edit course form

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,22 +1,27 @@
 module CoursesHelper
 
-
     def conditionally_display_heading(user)
         if params[:user_id]
             tag.h1 " Here are #{user.full_name}'s Courses" 
-        else
+    
             tag.h1 'Here are our courses'
         end
     end
 
-    
     def create_new_course_if_admin
         if logged_in? && current_user.admin?
             tag.div class: "button" do
                 link_to 'Create a new Course', new_course_path
             end
-            # content_tag(:div, link_to 'Create a new Course', new_course_path, class: "button" )
         end
+    end
+
+    def conditionally_present_teacher_fields
+        if params[:user_id]
+             f.hidden_field :teacher_id 
+        else
+            "Teacher:  #{f.collection_select(:teacher_id, User.teacher, :id, :full_name, prompt: "Select a Teacher ")}"
+        end 
     end
 
 end
