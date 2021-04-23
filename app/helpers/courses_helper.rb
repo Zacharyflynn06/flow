ea6770c1086fe53d6d81e676aa85ba90dbe4c430
Zac <zacharyflynn06@gmail.com>
2021-04-23 15:24:56 -0600

    fix review helper

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -48,17 +48,17 @@ module CoursesHelper
         if !course.reviews.empty?
             tag.div class: "wrapper" do
                 concat tag.h3 "Reviews"
-    
-                concat tag.ul do
-                    concat course.reviews.collect do |review|
-                        concat tag.li review.student.full_name
-                        concat tag.li review.rating
-                        concat tag.li review.content
-                        # if logged_in? && review.student = current_user
-                        #     concat link_to 'Edit my review', edit_review_path(review), id: review.id
-                        # end
+                concat tag.ul {
+                    course.reviews.collect do |review|
+                        concat content_tag :li, "Author: #{review.student.full_name}"
+                        concat content_tag :li, "Rating: #{review.rating}"
+                        concat content_tag :li, review.content
+                        if logged_in? && review.student == current_user
+                            concat link_to 'Edit my review', edit_review_path(review), id: review.id
+                        end
+                        concat tag.hr
                     end.join.html_safe
-                end
+                }
             end
         end
     end
