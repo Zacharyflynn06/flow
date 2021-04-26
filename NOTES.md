b17419de4e079aa513285c580d67bab23fc4e743
Zac <zacharyflynn06@gmail.com>
2021-04-26 13:02:00 -0600

    add footer

--- NOTES.md
+++ NOTES.md
@@ -2,10 +2,6 @@ Sign up form should be two columns
 
 Add a Footer
 
-Issues with reviews rendering from helper
-
-doesnt logout when i close the browser
-
 welcome helper
 
 why does my errors change
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
+
+    def conditionaly_present_nav_buttons
+        if !logged_in?
+            concat tag.li {
+                link_to 'Login', login_path, class: "nav-bar-float-right"
+            }
+            tag.li {
+                 link_to 'Sign Up', new_user_path, class: "nav-bar-float-right"
+            }
+        else
+            tag.li {
+                link_to "Logout", logout_path, method: "post", class: "nav-bar-float-right"
+            }
+        end
+    end
+
 end
 
